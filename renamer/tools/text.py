a041c809eb3b86d293ff3eff0e240a5c2ac0aea4
GauravSharma2005 <87546389+GauravSharma2005@users.noreply.github.com>
2021-07-17 02:39:09 +0530

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -3,7 +3,7 @@ from pyrogram.emoji import *
 class TEXT:
     DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
     UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
-    UPLOAD_SUCESS = f"Thanks for using [me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)"
+    UPLOAD_SUCESS = f"Thanks for using [me](https://t.m/IAmironBoy)"
     BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
     NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
     SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
@@ -24,9 +24,9 @@ class TEXT:
 
 ** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
 
-** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+** Group:** [Ns BOT SUPPORT](https://t.me/HindiHDHungama)
 
-** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
+** Source Code:** [🖕]()
 """
 
     HELP_USER = """**Follow Below Steps:**
@@ -36,12 +36,13 @@ class TEXT:
 ☞︎︎︎ Now send me the Telegram file you want to rename.
 ☞︎︎︎ Send the new name when bot ask.
 
-For source code check about
+☞︎︎︎ Note :- Extension Required
 """
 
     START_TEXT = """Hi {user_mention},
 
 I am a telegram renamer with permanent thumbnail.
+Created By @IAmIronBoy 
 
 𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 𝖧𝖾𝗅𝗉.
 
@@ -57,6 +58,6 @@ of 20rs, 30rs, 50rs, 70rs, 100rs, 200rs
  
 __--Payment Methods:--__
  
-GooglePay / Paytm / PhonPay / paypal / Net Banking
+GooglePay / Paytm / PhonPay 
  
-**For Donate:** message me @MaxxRiderz"""
+**For Donate:** message me @iamironboy"""
